5839ae5d7e58938d7c9e1a30df2928c82c732ce4
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-03-16 22:52:54 -0300

    feat: apply charts styling

--- src/components/message-volume/messageVolumeChart.js
+++ src/components/message-volume/messageVolumeChart.js
@@ -18,8 +18,45 @@ export default function MessageVolumeChart() {
             x: { set: ["Months"] },
           },
         },
+        style: {
+          plot: {
+            marker: {
+              colorPalette: '#ff6a6a' // stackup color
+            },
+            overflow: "visible",
+            xAxis: {
+              color: '#d1d5db', // gray-300
+              label: {
+                color: '#6b7280', // gray-500
+                fontFamily: 'Poppins',
+                fontSize: '0.875em'
+              }
+            },
+            yAxis: {
+              interlacing: {
+                color: "#f3f4f6" //gray-100
+              },
+              label: {
+                color: '#6b7280', // gray-500
+                fontFamily: 'Poppins',
+              },
+              title: {
+                fontSize: '0' // hides the yAxis title
+              }
+            }
+          },
+          logo: {
+            width: 0 // hides vizzu logo
+          },
+          tooltip: {
+            borderColor: '#ff6a6a', // stackup color
+            color: '#1e293b', // gray-900
+            layout: 'singleLine'
+          }
+        }
       })
-    );
+    )
+    .then((chart) => chart.feature("tooltip", true));
   }, []);
 
   return <canvas ref={canvasRef} id="myVizzu" style={{ width: "800px", height: "480px" }} />;
