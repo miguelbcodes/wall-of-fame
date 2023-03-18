69cef2b24c758f3cc676b5533301a82091ea4a45
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-03-18 12:43:47 -0300

    refactor: change the charts components to a single component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import ActivatedUsersChart from "./components/activated-users/activatedUsersChart";
-import MessageVolumeChart from "./components/message-volume/messageVolumeChart";
-import FoundSupportChart from "./components/found-support/foundSupportChart";
 import "./index.css";
+import ColumnChart from "./components/charts/columnChart";
 
 function App() {
   return (
     <main className="container font-poppins sm:mx-auto">
       <section className="flex flex-col items-center my-4">
         <h3 className="text-3xl font-bold text-gray-800 mb-1.5">Total activated Users</h3>
-        <ActivatedUsersChart />
+        <ColumnChart datafile={require('./data/hardStats.json')} dimensions={'month'} measures={'activated users'} />
       </section>
       <section className="flex flex-col items-center my-4">
         <h3 className="text-3xl font-bold text-gray-800 mb-1.5">Total message volume</h3>
         <p className="mb-3 text-gray-700 font-medium">Messages in our Discord channels: general chat, general help, quest help and forum, community collabs, safe space</p>
-        <MessageVolumeChart />
+        <ColumnChart datafile={require('./data/hardStats.json')} dimensions={'month'} measures={'message volume'}/>
       </section>
       <section className="flex flex-col items-center my-4">
         <h3 className="text-3xl font-bold text-gray-800 mb-1.5">Found support in community</h3>
         <p className="mb-3 text-gray-700 font-medium">Instances of Stackies who found support in the community</p>
-        <FoundSupportChart />
+        <ColumnChart datafile={require('./data/hardStats.json')} dimensions={'month'} measures={'stackies who found support in the community'}/>
       </section>
     </main>
   );
