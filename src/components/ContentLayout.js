dd776ef42eaac64ae7c489f963190ed9793c1cf6
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-03-21 22:54:46 -0300

    feat: basic page costumizations

--- src/components/ContentLayout.js
+++ src/components/ContentLayout.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function ContentLayout({ children }) {
     return (
-        <main className="bg-slate-100 h-[calc(100vh-4rem)]">
-            <section className="container mx-auto px-3 bg-white py-4 h-full grid grid-cols-8 grid-rows-2 gap-5">
+        <main className="h-[calc(100vh-4rem)] bg-white">
+            <section className="container mx-auto px-3 py-4 h-full grid grid-cols-8 grid-rows-2 gap-5">
                 { children }
             </section>
         </main>
