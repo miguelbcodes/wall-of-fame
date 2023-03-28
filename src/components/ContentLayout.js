8e84d84be727c9dc3a6565addb006e2ffd5400cd
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-03-27 22:59:26 -0300

    feat: update layout styles and switch tab navigation to arrow buttons

--- src/components/ContentLayout.js
+++ src/components/ContentLayout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ContentLayout({ children }) {
     return (
-        <main className="h-[calc(100vh-4rem)] bg-white">
+        <main className="h-full">
             <section className="container mx-auto px-3 py-4 h-full grid grid-cols-8 grid-rows-2 gap-5">
                 { children }
             </section>
--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -24,12 +24,28 @@ export default function PageLayout() {
 
     return (
         <>
-            <Header tabs={tabs} currentTab={currentTab} onTabClick={handleTabClick}/>
-            {tabs.map((tab, i) =>
-                <div key={i}>
-                    {currentTab === `${tab.id}` && tab.content}
-                </div>
-            )}
+            <div className="h-screen font-poppins p-4 relative bg-slate-100">
+                <div className="h-full w-full p-0.5 bg-stackup polygon-cyberpunk-wrapper">
+                    <div className="h-full w-full polygon-cyberpunk-frame p-12 bg-[rgb(255,240,240)]">
+                        {false && <Header tabs={tabs} currentTab={currentTab} onTabClick={handleTabClick}/>}
+                        {(tabs.filter(tab => `${tab.id}` === currentTab))[0].content}
+                    </div>
+                </div>  
+                {(tabs[(Number(currentTab) - 2)]) &&
+                    <button className="absolute w-5 h-5 top-[calc(50%-0.5rem)] left-4 group" onClick={() => handleTabClick(`${Number(currentTab) - 1}`)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="w-5 h-5 fill-gray-700 group-hover:fill-stackup">
+                            <path fillRule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                }
+                {tabs[currentTab] &&
+                    <button className="absolute w-5 h-5 top-[calc(50%-0.5rem)] right-4 group" onClick={() => handleTabClick(`${Number(currentTab) + 1}`)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="w-5 h-5 fill-gray-700 group-hover:fill-stackup">
+                            <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                }
+            </div>
         </>
     )
 }
