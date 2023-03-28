973a1d34f83fd3126976968f12e77b3fb2ec1962
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-03-28 05:48:05 -0300

    feat: update basic layout components

--- src/components/ContentLayout.js
+++ src/components/ContentLayout.js
@@ -3,9 +3,7 @@ import React from "react";
 export default function ContentLayout({ children }) {
     return (
         <main className="h-full">
-            <section className="container mx-auto px-3 py-4 h-full grid grid-cols-8 grid-rows-2 gap-5">
-                { children }
-            </section>
+            { children }
         </main>
     );
 }
