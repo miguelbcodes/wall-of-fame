fdbbba0c505ec7a7726de32b622615943d340a46
Miguel Bertol <miguelbertolcodes@gmail.com>
2023-04-04 21:27:14 -0300

    fix: typos

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -10,7 +10,7 @@ const tabs = [
         content: <AwardsPage />
     },
     {
-        tabTitle: 'Community Higlights',
+        tabTitle: 'Community Highlights',
         content: <CommunityPage />
     },
     {
